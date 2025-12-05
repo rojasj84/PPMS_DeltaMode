aa1607a28899a154b9e2156b75b9c8f65e2f7842
rojasj84 <rojasj84@gmail.com>
2025-12-05 15:39:04 -0500

    added GPIB behavior and some class structure to main

--- main.py
+++ main.py
@@ -52,6 +52,41 @@ class DataFile:
             print(self.time_data_array)
             print(self.temperature_data_array)
 
+class KeithleyConnect:
+    def __init__(self, address):
+        keithley_6112_address = "GPIB0::" + address + "::INSTR"
+        
+        #Initial connection to keithley and set up method of data gathering
+        gpib_comm = pyvisa.ResourceManager()
+        self.keithley_6112 = gpib_comm.open_resource(keithley_6112_address)
+        self.keithley_6112.write('*RST')
+        time.sleep(.5)
+        self.keithley_6112.write('SOUR:DELT:HIGH 1e-3') #Sets high source value to 1mA.
+        time.sleep(.5)
+        self.keithley_6112.write('SOUR:DELT:DELay 100e-3') # Sets Delta delay to 100ms.
+        time.sleep(.5)
+        self.keithley_6112.write('SOUR:DELT:COUN INF') # Sets Delta count to infinite.
+        time.sleep(.5)
+        self.keithley_6112.write('SOUR:DELT:CAB ON') # Enables Compliance Abort.
+        time.sleep(.5)
+        self.keithley_6112.write('TRAC:POIN 10') # Sets buffer to 10 points. A
+        time.sleep(.5)
+        self.keithley_6112.write('SOUR:DELT:ARM') # Arms Delta.
+        time.sleep(2)
+        self.keithley_6112.write('INIT:IMM')
+        time.sleep(1)
+        self.keithley_6112.write('UNIT OHMS')
+    
+    def obtain_current_data(self):
+        
+        #Obtain latest data from Keithley 
+        data_string = self.keithley_6112.query('SENS:DATA:FResh?')
+        #Parse the data string into a list of floating-point numbers
+        data_list = [float(x) for x in data_string.strip().split(',')]
+
+        print(data_list)
+        time.sleep(0.1)
+
 def UpdateNumbers():
     global file_path
     global last_modified    
@@ -66,21 +101,27 @@ def UpdateNumbers():
     #time.sleep(.1)  # Check every second    
     window.after(100, UpdateNumbers)
 
+def quit():
+    window.destroy()
+
 if __name__ == "__main__":
 
     window = tk.Tk()
     window.title("PPMS Data Gathering")
-    window.geometry("1000x400")
+    window.geometry("400x400")
 
-    file_path = os.path.abspath(filedialog.askopenfilename())
+
+    button_close_program = tk.Button(window, text="EXIT", command = quit)
+    button_close_program.place(x = 125, y = 10, width= 150, height= 30)
+
+    #log.askopenfilename())
+    file_path = "data/short_example.dat"
+    
+    # Load object to connect to Keithley via GPIB, address is 12
+    Keithley6221 = KeithleyConnect("12")
+
+    # Open dialog window that looks for the data file
     complete_file_path = os.path.abspath(file_path)
-
-    keithley_6112_address = "GPIB0::12::INSTR"  
-
-    gpib_comm = pyvisa.ResourceManager()
-    keithley_6112 = gpib_comm.open_resource(keithley_6112_address)
-    print(keithley_6112.query(':IDN?'))
-
     QD_DataFile = DataFile(complete_file_path)
 
     #print(QD_DataFile.time_data_array)
